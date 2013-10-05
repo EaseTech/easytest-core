9c3dbf261f310f06a04263408d26b69c838e777e
Anuj kumar <anuj.gandharv@gmail.com>
2013-10-05 20:26:48 +0200

    Removed duplicate annotation

--- src/test/java/org/easetech/easytest/example/TestErrorCollector.java
+++ src/test/java/org/easetech/easytest/example/TestErrorCollector.java
@@ -20,7 +20,7 @@ import org.junit.runner.RunWith;
  *
  */
 
-@Ignore
+
 @RunWith(DataDrivenTestRunner.class)
 @DataLoader(filePaths = { "classpath:org/easetech/data/testExcelData.xls" })
 @TestConfigProvider({TestConfigProviderClass.class})
